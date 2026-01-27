870135b8d7755579b98cd1166bdc41ac2cea57d9
stadrey wilson <stadreywilson@yahoo.com>
2026-01-27 13:48:36 -0600

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
-import { HashRouter } from "react-router-dom";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
